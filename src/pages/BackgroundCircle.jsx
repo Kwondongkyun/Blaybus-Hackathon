fdb75b4712273a158bb90742e71b98c6fe4fc3d1
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2025-02-16 19:32:06 +0900

    Delete src directory

--- src/pages/BackgroundCircle.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/BackgroundCircle.css';
-
-function BackgroundCircle(){
-    return(
-        <div className="background_circle"></div>
-    );
-}
-
-export default BackgroundCircle;
